04f9cfa94c012f5f6548a9da4bfdb56fb8f208f5
developers-houfy-1 <ajagtap9100@gmail.com>
2024-06-25 12:42:52 +0530

    Amazon Amazon

--- pages/componuts/Navbar/Navbar.js
+++ pages/componuts/Navbar/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar() {
 
       <div className='col-md-12'>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Amazon</a>
+          <a class="navbar-brand" href="#">AmazonAmazon</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
